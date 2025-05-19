ca56744e540f014bc18e457edbfb0107f7c3d794
Serge Zaugg <serge.zaugg@gmail.com>
2025-05-19 19:27:15 +0200

    quickfix - new png plot

--- page01.py
+++ page01.py
@@ -10,7 +10,7 @@ c00, c01  = st.columns([0.1, 0.18])
 
 with c00:
     with st.container(border=True) : 
-        st.header("Data flow chart")
+        # st.header("Data flow chart")
         st.image(image = "pics/data_flow_chart_2.png", caption="Data flow chart", width=None, 
                  use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=False)
          
