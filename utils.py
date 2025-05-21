5b16e92f143b07b93453f2ce04914a25b7c09630
Serge Zaugg <serge.zaugg@gmail.com>
2025-05-21 12:48:45 +0200

    let's see if optics eats less memory ...

--- utils.py
+++ utils.py
@@ -12,9 +12,8 @@ import pandas as pd
 import plotly.express as px
 import umap.umap_ as umap
 from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import DBSCAN, OPTICS
-
-
+# from sklearn.cluster import DBSCAN
+from sklearn.cluster import OPTICS
 
 def update_ss(kname, ssname):
     """
@@ -66,7 +65,6 @@ def perform_dbscan_clusterin(X, eps, min_samples):
     """
     # clu = DBSCAN(eps = eps, min_samples = min_samples, metric='euclidean', n_jobs = 4) 
     clu = OPTICS(min_samples=min_samples, max_eps=eps, metric='minkowski', n_jobs=-1)
-
     clusters_pred = clu.fit_predict(X)
     return(clusters_pred)
 
